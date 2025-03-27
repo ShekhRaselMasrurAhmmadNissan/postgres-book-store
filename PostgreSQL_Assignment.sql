a1e20df1170e728187e051909eaafa9c8e31fa3b
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2025-03-27 06:04:03 +0600

    add: create books table with validation constraints

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -4,3 +4,16 @@
 -- Database Creation
 CREATE DATABASE bookstore_db;
 
+-- Create Books Table
+CREATE TABLE books(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(50),
+	author VARCHAR(50),
+	price INT,
+	CHECK (price > 0),
+	stock INT,
+	CHECK (stock >= 0),
+	published_year CHAR(4)
+)
+
+
